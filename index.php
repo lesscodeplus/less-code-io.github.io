9c8ea86a5ccc305d289a70e61ed7d5c33d1e50ea
supuncodes <supuncodes@gmail.com>
2020-01-25 15:56:19 +0530

    Automatic redirection added

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+    $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+    header('HTTP/1.1 301 Moved Permanently');
+    header('Location: ' . $location);
+    exit;
+}
+
 $requestUri = isset($_SERVER["REDIRECT_URL"]) ? $_SERVER["REDIRECT_URL"]: $_SERVER["REQUEST_URI"];
 $requestUri = str_replace("less-code/","", $requestUri);
 $basePath = dirname(__FILE__);
